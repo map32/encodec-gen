7d08a428cc88d0a5ec5ff65bd340c9e043e26871
freepina <freepina@gmail.com>
2022-11-28 22:27:16 -0500

    Update train.py

--- train.py
+++ train.py
@@ -6,13 +6,13 @@ from torch.utils.data import random_split
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning import Trainer
-from deepspeed.ops.adam import DeepSpeedCPUAdam
 from sampler import Sampler
 from mega_transformer import Mega
 from data_utils import *
+import pickle
 
 class LitAutoEncoder(pl.LightningModule):
-    def __init__(self, codebook_size=1024, codebook_num=8, length_per_codebook=512, model_dim=128, model_depth = 6, num_chunks = 2, steps=50, batch_size=4, lr=1e-4):
+    def __init__(self, codebook_size=1024, codebook_num=8, length_per_codebook=512, model_dim=128, model_depth = 6, num_chunks = 2, steps=50, batch_size=4, lr=5e-4):
         super().__init__()
         token_size = codebook_size
         max_len = length_per_codebook*codebook_num
@@ -36,7 +36,7 @@ class LitAutoEncoder(pl.LightningModule):
     def forward(self, x, step = None):
       t = torch.randint(1,self.sampler.steps+1,(1,)).item() if step is None else step
       x = self.denoiser.preprocess(x)
-      nmask = torch.zeros_like(x) * self.sampler.mask_id
+      nmask = torch.randint_like(x,self.sampler.token_size-1)
       x = x.view(x.size(0), -1)
       x_masked = self.sampler.add_noise(x,t,nmask)
       x_pred = self.denoiser(x)
@@ -45,21 +45,17 @@ class LitAutoEncoder(pl.LightningModule):
       return t,nmask,x,x_masked,x_pred,loss
 
     def configure_optimizers(self):
-      #return torch.optim.AdamW(self.parameters(), lr=self.lr)
-      return DeepSpeedCPUAdam(self.parameters(), lr=self.lr)
+      return torch.optim.AdamW(self.parameters(), lr=self.lr)
+      #return DeepSpeedCPUAdam(self.parameters(), lr=self.lr)
 
     def training_step(self, train_batch, batch_idx):
       x = train_batch.tile((batch_size,1,1))
       t = torch.randint(1,self.sampler.steps+1,(1,))
       x = self.denoiser.preprocess(x)
-      print(t)
-      print(x[:,:32])
-      nmask = torch.zeros_like(x) * self.sampler.mask_id
+      nmask = torch.randint_like(x,self.sampler.token_size-1)
       x = x.view(x.size(0), -1)
       x_masked = self.sampler.add_noise(x,t,nmask)
-      print(x_masked[:,:32])
       x_pred = self.denoiser(x_masked).permute(0,2,1)
-      print(x_pred[:,:,:32])
       loss = self.criterion(x_pred,x)
       self.log('train_loss', loss)
       return loss
@@ -68,7 +64,7 @@ class LitAutoEncoder(pl.LightningModule):
       x = val_batch.tile((batch_size,1,1))
       t = torch.randint(1,self.sampler.steps+1,(1,))
       x = self.denoiser.preprocess(x)
-      nmask = torch.zeros_like(x) * self.sampler.mask_id
+      nmask = torch.randint_like(x,self.sampler.token_size-1)
       x = x.view(x.size(0), -1)
       x_masked = self.sampler.add_noise(x,t,nmask)
       x_pred = self.denoiser(x_masked).permute(0,2,1)
@@ -87,7 +83,7 @@ codebook_size=1024
 codebook_num=8
 length=1024
 batch_size=4
-model_dim = 80
+model_dim = 128
 model_depth = 1
 chunk_size = 256
 num_chunks = length*codebook_num // chunk_size
@@ -101,13 +97,31 @@ model_dim = model_dim,
 model_depth = model_depth,
 num_chunks = num_chunks)
 
-with torch.amp.autocast(device_type='cuda', dtype=torch.float16):
-    loss = model.denoiser(torch.randint(0,codebook_num*codebook_size+1,(1,length*codebook_num)))
-    print(loss)
+#with torch.amp.autocast(device_type='cuda', dtype=torch.float16):
+#    loss = model.denoiser(torch.randint(0,codebook_num*codebook_size+1,(1,length*codebook_num)))
+#    print(loss)
+#
+#quit()
 
-quit()
+cached_path = '/content/saved_encodings'
 
-data_list = getData('/content/drive/MyDrive/wave_encodec/',length)
+
+try:
+    file = open(cached_path, 'rb')
+    data_list = pickle.load(file)
+    print(data_list)
+    file.close()
+except:
+    print('why')
+    data_list = getData('/content/drive/MyDrive/wave_encodec/',length)
+    file = open(cached_path, 'wb')
+    # dump information to that file
+    pickle.dump(data_list, file)
+    # close the file
+    file.close()
+
+print(data_list[0])
+
 d = EnCodecData(data_list)
 print(len(data_list))
 
@@ -119,14 +133,8 @@ val_loader = DataLoader(val, batch_size=1, shuffle=False)
 trainer = Trainer(
     accelerator='gpu',
     devices=1,
-    strategy='deepspeed_stage_2_offload',
     precision=16,
-    max_epochs=1,
-    gradient_clip_val=0.5,
-    auto_lr_find=False,
-    overfit_batches=10,
-    track_grad_norm=2,
-    log_every_n_steps=1
+    max_epochs=1
 )
 
 
