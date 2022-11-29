b7381a859c52776bcc9f8cb1019410695dae02c1
freepina <freepina@gmail.com>
2022-11-29 10:54:09 -0500

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -4,6 +4,7 @@ import torch
 import glob
 from pathlib import Path
 from torch.utils.data import Dataset
+from numpy.random import default_rng
 
 def getData(path,length,fraction=1.0):
     path = path
@@ -40,11 +41,35 @@ def getData(path,length,fraction=1.0):
         del t
     return dic
 
+def augment_data(length, codebook_dim=8):
+    def to_return(data_list):
+        l = []
+        for data in data_list:
+            data = data[:codebook_dim,:]
+            i = -1 if data.size(-1)%length == 0 else None
+            l += data.split(length,dim=-1)[:i]
+        return l
+    return to_return
+
+class RepeatingSampler():
+    def __init__(self, data_source, batch_size, shuffle=False) -> None:
+        self.data_source = data_source
+        self.b = batch_size
+        self.rng = default_rng() if shuffle else None
+    def __iter__(self):
+        tmp = np.repeat(np.arange(len(self.data_source)),self.b).reshape((-1,self.b))
+        if self.rng is not None:
+            self.rng.shuffle(tmp,axis=0)
+        for i in range(len(self.data_source)):
+            yield tmp[i,:]
+    def __len__(self) -> int:
+        return len(self.data_source)
+
 class EnCodecData(Dataset):
-    def __init__(self, data_list):
+    def __init__(self, data_list, transforms=None):
         self.dic = data_list
-
-
+        if transforms is not None:
+            self.dic = transforms(data_list)
     def __len__(self):
         return len(self.dic)
 
