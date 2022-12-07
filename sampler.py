3d75cda4ec5175da47a14403d726f2e5b525d8d2
freepina <freepina@gmail.com>
2022-12-06 23:42:14 -0500

    Update sampler.py

--- sampler.py
+++ sampler.py
@@ -12,7 +12,7 @@ class Sampler(nn.Module):
       self.L = max_len
       self.mask_id = token_size
       self.beta = 0.9
-      self.register_buffer('ind_num', (self.gamma(torch.arange(0,51))*self.L).round().long().clamp_min(0))
+      self.register_buffer('ind_num', (self.gamma(torch.arange(0,self.steps+1))*self.L).round().long().clamp_min(0))
       self.register_buffer('mask',torch.ones(self.batch_size,self.L).long() * self.mask_id)
   def sample_t(self,t=None) -> torch.Tensor:
       if t == -1:
@@ -41,21 +41,34 @@ class Sampler(nn.Module):
     mask = self.mask
     if noise_mask is None:
         noise_mask = torch.randint(0,self.token_size,(self.batch_size,self.L)).to(x)
-    mm = torch.ones_like(mask)
+    mm = torch.zeros_like(mask,dtype=torch.bool)
     if self.L > indices_to_pick:
         idx = torch.sort(torch.randint(
             0, self.L - indices_to_pick, (self.batch_size, indices_to_pick)
         ), axis=1).values + torch.arange(0, indices_to_pick).reshape(1, -1)
         idx = idx.to(x)
-        x = torch.scatter(x,dim=-1,index=idx, src=mask)
+        mm = torch.scatter(mm,dim=-1,index=idx,src=~mm)
+        x2 = mm * self.mask_id + ~mm * x
     else:
-        x = self.mask.clone()
-        idx = torch.arange(0, indices_to_pick).reshape(1, -1).to(x)
+        idx = torch.arange(0, indices_to_pick).reshape(1, -1)
+        idx = idx.to(x)
+        mm = ~mm
+        x2 = mm * self.mask_id
     if noised_indices > 0:
         idx2 = torch.multinomial(torch.ones(self.batch_size, indices_to_pick),noised_indices)
         idx2 = idx2.to(x)
-        x2 = torch.scatter(x,dim=-1,index=idx[torch.arange(len(idx)).unsqueeze(-1),idx2],src=noise_mask)
-    return x2,x
+        x2 = torch.scatter(x2,dim=-1,index=idx[torch.arange(len(idx)).unsqueeze(-1),idx2],src=noise_mask)
+    targets = mm * x + ~mm * self.mask_id
+    return x2,targets
   
   def __call__(self, x: torch.Tensor, step: int):
     return self.sample(x,step)
+  
+if __name__ =='__main__':
+    s = Sampler(steps=50,max_len=16,bn=1)
+    x = torch.randint(0,1024,(1,16))
+    nm = torch.ones_like(x) * -1
+    print(x)
+    print(s.add_noise(x,0,nm))
+    print(s.add_noise(x,25,nm))
+    print(s.add_noise(x,50,nm))
